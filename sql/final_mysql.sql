122f6a985fc41ee0d9227f56b7f14a30fdef9df3
34ERAT <philipkanuti@gmail.com>
2024-04-24 17:29:18 +0300

    removing sql  files

--- sql/final_mysql.sql
+++
@@ -1,7 +0,0 @@
-source /home/huble/Projects/church_rec_management_system/sql/tables.sql
-source /home/huble/Projects/church_rec_management_system/sql/batism_procedures.sql
-source /home/huble/Projects/church_rec_management_system/sql/clan_tribe_procedures.sql
-source /home/huble/Projects/church_rec_management_system/sql/couples_procedures.sql
-source /home/huble/Projects/church_rec_management_system/sql/death_procedures.sql
-source /home/huble/Projects/church_rec_management_system/sql/marriage_procedures.sql
-source /home/huble/Projects/church_rec_management_system/sql/persons_procedures.sql
