e16ae1c99926bf875991faf3b323d5778efae65e
34ERAT <philipkanuti@gmail.com>
2024-07-29 23:23:40 +0300

    remove the configure setup from  setup.py to church_rec_manager

--- church_rec_manager.py
+++ church_rec_manager.py
@@ -1,7 +1,39 @@
 from ui.App import App
+import os
+
+
+def setup():
+    if not os.path.exists('.env'):
+        print("\nConfiguring your application:")
+        db_host = input("Enter DB_HOST [localhost]: ") or "localhost"
+        db_port = input("Enter DB_PORT [3306]: ") or "3306"
+        db_user = input("Enter DB_USER [username]: ") or "username"
+        db_password = input("Enter DB_PASSWORD [password]: ") or "password"
+
+        config_content = f"""
+                DB_HOST={db_host}
+                DB="Church_db_2"
+                PORT={db_port}
+                DB_PASSWORD={db_password}
+                DB_USER={db_user}
+                BAPTISM_ARCHIVE="./.Archives/Baptism"
+                MARRIAGE_ARCHIVE="./.Archives/Marriage"
+                DEATH_ARCHIVE="./.Archives/Death"
+                """
+
+        with open('.env', 'w') as config_file:
+            config_file.write(config_content)
+
+        print("\nConfiguration completed successfully!")
+    else:
+        print("\n.env file already exists. Skipping configuration.")
+
+
 def main():
+    setup()
     app = App()
     app.mainloop()
+
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-class CustomInstallCommand(install):
-
-    def run(self):
-        # Call the standard run method first
-        install.run(self)
-        
-        # Ask the user for configuration values
-        print("\nConfiguring your application:")
-        db_host = input("Enter DB_HOST [localhost]: ") or "localhost"
-        db_port = input("Enter DB_PORT [3306]: ") or "3306"
-        db_user = input("Enter DB_USER [username]: ") or "username"
-        db_password = input("Enter DB_PASSWORD [password]: ") or "password"
-
-        config_content = f"""
-            DB_HOST={db_host}
-            DB="Church_db_2"
-            PORT={db_port}
-            DB_PASSWORD={db_password}
-            DB_USER={db_user}
-            BAPTISM_ARCHIVE="./.Archives/Baptism"
-            MARRIAGE_ARCHIVE="./.Archives/Marriage"
-            DEATH_ARCHIVE="./.Archives/Death"       
-            """
-        
-        with open('.env', 'w') as config_file:
-            config_file.write(config_content)
-        
-        print("\nConfiguration completed successfully!")
 
 setup(
     name='church_rec_manager',
@@ -86,8 +58,8 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    cmdclass={
-        'install': CustomInstallCommand,
-    }
+    # cmdclass={
+    #     'install': CustomInstallCommand,
+    # }
 )
 
