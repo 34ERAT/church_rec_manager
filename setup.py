001fb2a94bcf8b6dc4b24301501442b0047159f8
34ERAT <philipkanuti@gmail.com>
2024-07-29 18:50:32 +0300

    refractor of code

--- setup.py
+++ setup.py
@@ -38,39 +38,39 @@ setup(
     url='https://github.com/34ERAT/church_rec_management_system',
     packages=find_packages(),
     install_requires=[
-        "attrs==23.2.0"
-        "autopep8==2.3.1"
-        "Babel==2.15.0"
-        "contextlib2==21.6.0"
-        "CTkMessagebox==2.7"
-        "CTkTable==1.1"
-        "customtkinter==5.2.2"
-        "darkdetect==0.8.0"
-        "execnet==2.1.1"
-        "greenlet==3.0.3"
-        "iniconfig==2.0.0"
-        "jsonschema==4.23.0"
-        "jsonschema-specifications==2023.12.1"
-        "mock==5.1.0"
-        "msgpack==1.0.8"
-        "mysql-connector-python==9.0.0"
-        "packaging==24.1"
-        "path==17.0.0"
-        "path.py==12.5.0"
-        "pillow==10.4.0"
-        "pluggy==1.5.0"
-        "psutil==6.0.0"
-        "pycodestyle==2.12.0"
-        "PyMySQL==1.1.1"
-        "pynvim==0.5.0"
-        "pytest==8.3.2"
-        "pytest-shutil==1.7.0"
-        "python-dotenv==1.0.1"
-        "referencing==0.35.1"
-        "rpds-py==0.19.0"
-        "six==1.16.0"
-        "termcolor==2.4.0"
-        "tk==0.1.0"
+        "attrs==23.2.0",
+        "autopep8==2.3.1",
+        "Babel==2.15.0",
+        "contextlib2==21.6.0",
+        "CTkMessagebox==2.7",
+        "CTkTable==1.1",
+        "customtkinter==5.2.2",
+        "darkdetect==0.8.0",
+        "execnet==2.1.1",
+        "greenlet==3.0.3",
+        "iniconfig==2.0.0",
+        "jsonschema==4.23.0",
+        "jsonschema-specifications==2023.12.1",
+        "mock==5.1.0",
+        "msgpack==1.0.8",
+        "mysql-connector-python==9.0.0",
+        "packaging==24.1",
+        "path==17.0.0",
+        "path.py==12.5.0",
+        "pillow==10.4.0",
+        "pluggy==1.5.0",
+        "psutil==6.0.0",
+        "pycodestyle==2.12.0",
+        "PyMySQL==1.1.1",
+        "pynvim==0.5.0",
+        "pytest==8.3.2",
+        "pytest-shutil==1.7.0",
+        "python-dotenv==1.0.1",
+        "referencing==0.35.1",
+        "rpds-py==0.19.0",
+        "six==1.16.0",
+        "termcolor==2.4.0",
+        "tk==0.1.0",
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
